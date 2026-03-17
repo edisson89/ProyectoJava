1ef062717d7621d152627ec23898e601d3ea9e79
edisson89 <edisson.12@hotmail.com>
2026-03-16 21:24:53 -0500

    Agregando Hello World Java

--- src/miprimerproyectogit/MiPrimerProyectoGit.java
+++ src/miprimerproyectogit/MiPrimerProyectoGit.java
@@ -15,6 +15,7 @@ public class MiPrimerProyectoGit {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("hola world, desde netbean and git");
     }
     
 }
